96637583108040a7e57cbb000722dd98f15cd65b
sshcrack <34072808+sshcrack@users.noreply.github.com>
2025-06-24 11:41:38 +0200

    Base commit

---
+++ src/main/java/me/sshcrack/mc_talking/WebsocketLibMod.java
@@ -0,0 +1,9 @@
+package me.sshcrack.mc_talking;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(WebsocketLibMod.MODID)
+public class WebsocketLibMod {
+    public static final String MODID = "websocket_java_lib";
+    public WebsocketLibMod() {}
+}
